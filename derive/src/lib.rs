ef2c3499d65d043c0a306e684a6a013bce2f3fee
Gavrilikhin Daniil <gavrilikhin.daniil@gmail.com>
2023-06-18 15:11:54 +0800

    Add template for derive

--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -1,14 +1,35 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use darling::FromDeriveInput;
+use syn::{parse_macro_input, DeriveInput};
+
+#[derive(FromDeriveInput)]
+#[darling(attributes(rule))]
+struct RuleDeriveAttributes {
+    pub ident: syn::Ident,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+use quote::quote;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+/// Examples:
+/// ```
+/// use derive::Rule;
+///
+/// #[derive(Rule)]
+/// #[rule("{username}:{password}")]
+/// struct Credentials {
+/// 	#[pattern("/[_0-9a-zA-Z]+/")]
+/// 	pub username: String,
+///     #[pattern("/[_0-9a-zA-Z]+/")]
+/// 	pub password: String
+/// }
+/// ```
+#[proc_macro_derive(Rule, attributes(rule, pattern))]
+pub fn rule(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+
+    RuleDeriveAttributes::from_derive_input(&input)
+        .map(|args| {
+            let expanded = quote! {};
+            proc_macro::TokenStream::from(expanded)
+        })
+        .unwrap_or_else(|e| e.write_errors().into())
 }
